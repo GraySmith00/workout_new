e681fdbe84ba13f4258eab52c611bec9dfd3199f
Gray Smith <gray.smith00@gmail.com>
2016-12-06 00:51:45 +0000

    add first and last name to signup form and whitelist params

--- spec/features/signing_users_up_spec.rb
+++ spec/features/signing_users_up_spec.rb
@@ -7,6 +7,8 @@ RSpec.feature "Users signup" do
        visit '/'
        
        click_link "Sign up"
+       fill_in "First name", with: "John"
+       fill_in "Last name", with: "Doe"
        fill_in "Email", with: "user@example.com"
        fill_in "Password", with: "12345678"
        fill_in "Password confirmation", with: "12345678"
@@ -14,7 +16,9 @@ RSpec.feature "Users signup" do
        click_button "Sign up"
        
        expect(page).to have_content("Welcome! You have signed up successfully.")
-        
+       
+       visit "/"   
+       expect(page).to have_content("John Doe")
     end
     
 end
