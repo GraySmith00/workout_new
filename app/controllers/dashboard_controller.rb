08635c33e5354c9a11011710302da704cd161abe
Gray Smith <gray.smith00@gmail.com>
2016-12-06 06:38:40 +0000

    implement user search

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,5 +4,9 @@ class DashboardController < ApplicationController
     @athletes = User.paginate(:page => params[:page])
   end
   
+  def search
+    @athletes = User.search_by_name(params[:search_name]).paginate(:page => params[:page])
+  end
+  
   
 end
